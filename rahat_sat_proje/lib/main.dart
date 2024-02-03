2f688d40bcff26d9599ecdc8c658989d6fd6889d
dilarabehar <dilarabehar71@gmail.com>
2024-02-03 09:25:15 +0300

    Committing local changes before pull

--- rahat_sat_proje/lib/main.dart
+++ rahat_sat_proje/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:rahat_sat_project/features/colors.dart';
 import 'package:rahat_sat_project/model/autho_response.dart';
 import 'package:rahat_sat_project/model/login_model.dart';
 import 'package:rahat_sat_project/model/product_model.dart';
-import 'package:rahat_sat_project/screens/products.dart';
+import 'package:rahat_sat_project/screens/product.dart';
 import 'package:rahat_sat_project/services/user_client.dart';
 import 'package:flutter/material.dart';
 
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       darkTheme: ThemeData(brightness: Brightness.dark),
-      home:    MyHomePage(title: 'Flutter Demo Home Page'),
+      home:   const ProductList(),
     );
   }
 }
